3e52d598a2c416ba656f8cac4ea81d27bd588e08
GILLMtz <martinezgomezgil@hotmail.com>
2022-12-13 22:11:56 -0600

    refactor day-07

--- src/day-07/index.js
+++ src/day-07/index.js
@@ -1,11 +1,15 @@
 function getGiftsToRefill(a1, a2, a3) {
     return [... (new Set([].concat(a1, a2, a3)))]
-        .filter(gift =>
-            (a1.includes(gift) && !(a2.includes(gift) || a3.includes(gift))) ||
-            (a2.includes(gift) && !(a1.includes(gift) || a3.includes(gift))) ||
-            (a3.includes(gift) && !(a1.includes(gift) || a2.includes(gift)))
-        );
-}
+      .filter(gift => {
+        let b1 = a1.includes(gift);
+        let b2 = a2.includes(gift);
+        let b3 = a3.includes(gift)
+        return (b1 && !(b2 || b3)) ||
+          (b2 && !(b1 || b3)) ||
+          (b3 && !(b1 || b2))
+      }
+      );
+}  
 
 const a1 = ['bici', 'coche', 'bici', 'bici']
 const a2 = ['coche', 'bici', 'muñeca', 'coche']
